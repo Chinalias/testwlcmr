da67814f1c7e4a9dbe452384078e65c42e4fbc81
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-09-08 20:22:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,47 +109,44 @@ client.on('guildMemberAdd',async member => {
 
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
 }
 client.on("ready", () => {
     var guild;
     while (!guild)
-        guild = client.guilds.get("480375166615027722");
+        guild = client.guilds.find("name", "test bots")
     guild.fetchInvites().then((data) => {
         data.forEach((Invite, key, map) => {
             var Inv = Invite.code;
             dat[Inv] = Invite.uses;
-        });
-    });
-});
- 
- 
- 
+        })
+    })
+})
 client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("480375166615027722");
+    let channel = member.guild.channels.find('name', '480734524796305428');
     if (!channel) {
-        console.log("!the channel id it's not correct");
+        console.log("!channel fails");
         return;
     }
     if (member.id == client.user.id) {
         return;
     }
-    console.log('-');
+    console.log('made it till here!');
     var guild;
     while (!guild)
-        guild = client.guilds.get("480734524796305428");
+        guild = client.guilds.find("name", "test bots")
     guild.fetchInvites().then((data) => {
         data.forEach((Invite, key, map) => {
             var Inv = Invite.code;
             if (dat[Inv])
                 if (dat[Inv] < Invite.uses) {
-                    setTimeout(function() {
- channel.send(`**invited by** ${Invite.inviter} `) ;
-                    },1500);
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
  }
             dat[Inv] = Invite.uses;
-       
-       });
-    });
+        })
+    })
 });
+
 client.login(process.env.BOT_TOKEN);
